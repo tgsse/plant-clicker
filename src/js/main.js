ed9d1f82504c4ce31a2ab2fafa16c18c925b4a85
tgsse <tamas.gura.sse@gmail.com>
2023-08-24 22:28:04 +0200

    Refactor into MVC constants & iife

--- src/js/main.js
+++ src/js/main.js
@@ -1,78 +1,88 @@
-"use strict"
+(function() {
 
-const imagesDir = "assets/images/"
+	// Constants
 
-class DomManager {
-	updatePlantCard = (plant) => {
-		this.updatePlantName(plant.name)
-		this.updateCounter(plant.counter)
-		this.updateImage(plant)
+	const imagesDir = "assets/images/"
+
+	// Utility
+
+	function $(id) {
+		return document.getElementById(id)
 	}
 
-	updatePlantName = (value) => {
-		$("name").innerText = value
+	const viewUtils = {
+		buildPlantImage: (plant, onClick) => {
+			const img = document.createElement("img")
+			img.src = `${imagesDir}${plant.getImage()}`
+			img.alt = "plant"
+			img.style.cursor = "pointer"
+			img.addEventListener("click", event => onClick(event, plant))
+			return img
+		},
 	}
-	
-	updateCounter = (value) => {
-		$("counter").innerText = value
+
+	// App
+
+	const view = {
+		updatePlantCard: (plant) => {
+			view.updatePlantName(plant.name)
+			view.updateCounter(plant.counter)
+			view.updateImage(plant)
+		},
+
+		updatePlantName: (value) => {
+			$("name").innerText = value
+		},
+
+		updateCounter: (value) => {
+			$("counter").innerText = value
+		},
+
+		updateImage: (plant) => {
+			$("plantImageContainer").replaceChildren(viewUtils.buildPlantImage(plant, event => view.onPlantClick(event, plant)))
+		},
+
+		showPlantCollection: (plants) => {
+			const plantCollection = plants.map(plant => {
+				return viewUtils.buildPlantImage(plant, (_) => {
+					controller.state.selectedPlantId = plant.id
+					view.updatePlantCard(plant)
+				})
+			})
+			$("plantCollectionContainer").replaceChildren(...plantCollection)
+		},
+
+		onPlantClick: (event, plant) => {
+			if (plant.stage < plant.maxStage && ++plant.counter >= plant.stage * 3 + 3) {
+				controller.evolvePlant(plant)
+				view.updatePlantCard(plant)
+			}
+			view.updateCounter(plant.counter)
+		},
 	}
 
-	updateImage = (plant) => {
-		$("plantImageContainer").replaceChildren(domManager.buildPlantImage(plant, event => onPlantClick(event, plant)))
+	const model = {
+		plants: [
+			new Plant("plant1"),
+			new Plant("plant2"),
+		],
 	}
 
-	buildPlantImage = (plant, onClick) => {
-		const img = document.createElement("img")
-		img.src = `${imagesDir}${plant.getImage()}`
-		img.alt = "plant"
-		img.style.cursor = "pointer"
-		img.addEventListener("click", event => onClick(event, plant))
-		return img
+	const controller = {
+		state: {
+			selectedPlantId: undefined,
+		},
+
+		init: () => {
+			view.showPlantCollection(model.plants)
+		},
+
+		evolvePlant: (plant) => {
+			plant.stage++
+			view.showPlantCollection(model.plants)
+		}
 	}
-}
 
-const plants = [
-	new Plant("plant1"),
-	new Plant("plant2"),
-	]
+	controller.init()
 
-const state = {
-	selectedPlantId: undefined,
-}
-
-const domManager = new DomManager()
-
-init()
-
-function init() {
-	addPlantCollection()
-}
-
-function onPlantClick(event, plant) {
-	if (plant.stage < plant.maxStage && ++plant.counter >= plant.stage * 3 + 3) {
-		plant.stage++
-		domManager.updatePlantCard(plant)
-		addPlantCollection()
-	}
-	domManager.updateCounter(plant.counter)
-}
-
-function addPlantCollection() {
-	const plantCollection = plants.map(plant => {
-		const img = domManager.buildPlantImage(plant, (event) => {
-			console.log("buildimg", {plant, event})
-			state.selectedPlantId = plant.id
-			domManager.updatePlantCard(plant)
-		})
-		return img
-	})
-	$("plantCollectionContainer").replaceChildren(...plantCollection)
-}
-
-
-// Utility
-
-function $(id) {
-	return document.getElementById(id)
-}
-
+})()
