6f25a902d7b5442e618ff079f229a205de84b764
tgsse <tamas.gura.sse@gmail.com>
2023-08-25 11:39:43 +0200

    Added edit name functionality

--- src/js/main.js
+++ src/js/main.js
@@ -24,18 +24,34 @@
 	// App
 
 	const view = {
+		uiState: {
+			isEditorVisible: false,
+		},
 		plantNameEl: undefined,
 		plantCounterEl: undefined,
 		plantImageEl: undefined,
 		plantDateEl: undefined,
 		plantCollectionEl: undefined,
+		editBtnEl: undefined,
+		editorEl: undefined,
+		editorSubmitBtnEl: undefined,
+		editNameField: undefined,
+
 		init: () => {
 			view.plantNameEl = $("name")
 			view.plantCounterEl = $("counter")
 			view.plantImageEl = $("plantImageContainer")
 			view.plantDateEl = $("plantDate")
 			view.plantCollectionEl = $("plantCollectionContainer")
+			view.editBtnEl = $("editBtn")
+			view.editorEl = $("editor")
+			view.editorSubmitBtnEl = $("editorSubmitBtn")
+			view.editNameField = $("editNameField")
+
+			view.editBtnEl.addEventListener("click", view.onEditorToggle)
+			view.editorEl.addEventListener("submit", event => event.preventDefault())
 		},
+
 		updatePlantCard: (plant) => {
 			view.updatePlantName(plant.name)
 			view.updateCounter(plant.counter)
@@ -59,6 +75,10 @@
 			view.plantDateEl.innerText = new Date(createdOn)
 		},
 
+		clearNameInput: () => {
+			view.editorEl.reset()
+		},
+
 		showPlantCollection: (plants) => {
 			const plantCollection = plants.map(plant => {
 				const li = document.createElement("li")
@@ -79,6 +99,29 @@
 			}
 			view.updateCounter(plant.counter)
 		},
+
+		onEditorToggle: () => {
+			if (controller.state.selectedPlantId === undefined) {
+				alert("Trying to name something that isn't there is like trying to find a unicorn in a hamster's hideout – you're in for a legendary quest, but all you'll find are tiny paw prints and a bunch of confused hamsters! 🦄🐹")
+				return
+			}
+			if (view.uiState.isEditorVisible) {
+				view.editorSubmitBtnEl.removeEventListener("click", view.onEditorSubmit)
+				view.clearNameInput()
+				view.editorEl.classList.add("hidden")
+				view.uiState.isEditorVisible = false
+			} else {
+				view.editorSubmitBtnEl.addEventListener("click", view.onEditorSubmit)
+				view.editorEl.classList.remove("hidden")
+				view.uiState.isEditorVisible = true
+			}
+		},
+
+		onEditorSubmit: (event) => {
+			event.preventDefault()
+			const newName = view.editNameField.value
+			controller.updateName(newName)
+		}
 	}
 
 	const model = {
@@ -101,6 +144,17 @@
 		evolvePlant: (plant) => {
 			plant.stage++
 			view.showPlantCollection(model.plants)
+		},
+
+		updateName: (newName) => {
+			if (model.plants.find(p => p.name === newName)) {
+				alert("Just like a rare orchid, that name's got a 'Do Not Pick' sign – it's been plucked by someone else!")
+				view.clearNameInput()
+				return
+			}
+			model.plants.find(p => p.id === controller.state.selectedPlantId).name = newName
+			view.updatePlantName(newName)
+			view.onEditorToggle()
 		}
 	}
 
