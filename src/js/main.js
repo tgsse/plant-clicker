fa51c19673e63e3acc282c0ac879a71e0a37d641
tgsse <tamas.gura.sse@gmail.com>
2023-08-26 19:26:43 +0200

    Improved visuals

--- src/js/main.js
+++ src/js/main.js
@@ -26,6 +26,7 @@
 	const view = {
 		uiState: {
 			isEditorVisible: false,
+			isDetailsVisible: false,
 		},
 		plantNameEl: undefined,
 		plantCounterEl: undefined,
@@ -37,8 +38,10 @@
 		editorSubmitBtnEl: undefined,
 		editNameField: undefined,
 
-		init: () => {
+		init: (plants) => {
 			view.plantNameEl = $("name")
+			view.plantNameLabelEl = $("plantNameLabel")
+			view.plantDetailsSectionEl = $("plantDetailsSection")
 			view.plantCounterEl = $("counter")
 			view.plantImageEl = $("plantImageContainer")
 			view.plantDateEl = $("plantDate")
@@ -50,17 +53,24 @@
 
 			view.editBtnEl.addEventListener("click", view.onEditorToggle)
 			view.editorEl.addEventListener("submit", event => event.preventDefault())
+
+			view.showPlantCollection(plants)
 		},
 
 		updatePlantCard: (plant) => {
-			view.updatePlantName(plant.name)
+			view.updateName(plant.name)
 			view.updateCounter(plant.counter)
 			view.updateImage(plant)
 			view.updateDate(plant.createdOn)
+			if (!view.uiState.isDetailsVisible) {
+				view.uiState.isDetailsVisible = true
+				view.plantDetailsSectionEl.style.display = "flex"
+			}
 		},
 
-		updatePlantName: (value) => {
+		updateName: (value) => {
 			view.plantNameEl.innerText = value
+			view.plantNameLabelEl.innerText = value ? value : "nothing"
 		},
 
 		updateCounter: (value) => {
@@ -68,11 +78,11 @@
 		},
 
 		updateImage: (plant) => {
-			view.plantImageEl.replaceChildren(viewUtils.buildPlantImage(plant, event => view.onPlantClick(event, plant)))
+			view.plantImageEl.replaceChildren(viewUtils.buildPlantImage(plant, event => view.onPlantSelected(event, plant)))
 		},
 
 		updateDate: (createdOn) => {
-			view.plantDateEl.innerText = new Date(createdOn)
+			view.plantDateEl.innerText = new Date(createdOn).toDateString()
 		},
 
 		clearNameInput: () => {
@@ -86,13 +96,14 @@
 					controller.state.selectedPlantId = plant.id
 					view.updatePlantCard(plant)
 				})
+				img.classList.add("plantPreview")
 				li.append(img)
 				return li
 			})
 			view.plantCollectionEl.replaceChildren(...plantCollection)
 		},
 
-		onPlantClick: (event, plant) => {
+		onPlantSelected: (event, plant) => {
 			if (plant.stage < plant.maxStage && ++plant.counter >= plant.stage * 3 + 3) {
 				controller.evolvePlant(plant)
 				view.updatePlantCard(plant)
@@ -120,15 +131,19 @@
 		onEditorSubmit: (event) => {
 			event.preventDefault()
 			const newName = view.editNameField.value
-			controller.updateName(newName)
+			controller.updatePlantName(newName)
 		}
 	}
 
 	const model = {
-		plants: [
-			new Plant("plant1"),
-			new Plant("plant2"),
-		],
+		plants: [],
+
+		init: () => {
+			model.plants = [
+				new Plant("plant1"),
+				new Plant("plant2"),
+			]
+		}
 	}
 
 	const controller = {
@@ -137,8 +152,8 @@
 		},
 
 		init: () => {
-			view.init()
-			view.showPlantCollection(model.plants)
+			model.init()
+			view.init(model.plants)
 		},
 
 		evolvePlant: (plant) => {
@@ -146,14 +161,17 @@
 			view.showPlantCollection(model.plants)
 		},
 
-		updateName: (newName) => {
-			if (model.plants.find(p => p.name === newName)) {
+		updatePlantName: (newName) => {
+			if (newName.length === 0) {
+				alert("Just like how a squirrel named Steve or a toaster named Tina adds a sprinkle of excitement to life, your plant could use a dash of that too!")
+				return
+			} else if (model.plants.find(p => p.name === newName)) {
 				alert("Just like a rare orchid, that name's got a 'Do Not Pick' sign – it's been plucked by someone else!")
 				view.clearNameInput()
 				return
 			}
 			model.plants.find(p => p.id === controller.state.selectedPlantId).name = newName
-			view.updatePlantName(newName)
+			view.updateName(newName)
 			view.onEditorToggle()
 		}
 	}
